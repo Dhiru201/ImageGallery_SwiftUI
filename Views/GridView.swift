76f9646dd08d876d327fe089099ab5afb17ea83d
Dhirendra Kumar Verma <dhirendraverma07@gmail.com>
2024-10-17 15:36:18 +0530

    image grid screen created.

--- Views/GridView.swift
+++ Views/GridView.swift
@@ -7,14 +7,33 @@
 
 import SwiftUI
 
+/// A view that displays a single image, with loading and error states.
 struct GridView: View {
+    @StateObject private var imageLoader = ImageLoader()
+    let urlString: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct GridView_Previews: PreviewProvider {
-    static var previews: some View {
-        GridView()
+        GeometryReader { geometry in
+            ZStack {
+                Color.gray.opacity(0.3)
+                if let image = imageLoader.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .clipped()
+                        .transition(.opacity)
+                } else {
+                    ProgressView()
+                }
+            }
+        }
+        .aspectRatio(1, contentMode: .fit)
+        .onAppear {
+            imageLoader.load(from: urlString)
+        }
+        .onDisappear {
+            imageLoader.cancel()
+        }
     }
 }
--- Views/ImageGridView.swift
+++ Views/ImageGridView.swift
@@ -7,14 +7,31 @@
 
 import SwiftUI
 
+/// A view that displays a grid of images.
 struct ImageGridView: View {
+    @StateObject private var viewModel = ImageGridViewModel()
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ImageGridView_Previews: PreviewProvider {
-    static var previews: some View {
-        ImageGridView()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: viewModel.columns, spacing: viewModel.spacing) {
+                    ForEach(Array(viewModel.images.enumerated()), id: \.element.id) { index, image in
+                        // Navigation link to the detail view for each image
+                        NavigationLink(destination: ImageDetailView(images: viewModel.images, currentIndex: index)) {
+                            GridView(urlString: image.thumbnailUrl)
+                        }
+                    }
+                }
+                .padding(viewModel.spacing) // Padding around the grid
+            }
+            .background(Color.blue.opacity(0.3))
+            .navigationBarTitle(Constants.ImageGridView.NavigationTitle.rawValue , displayMode: .large)
+            .onAppear {
+                // Fetch images only if the image array is empty
+                if viewModel.images.isEmpty {
+                    viewModel.fetchImages()
+                }
+            }
+        }
     }
 }
